41574cef64e21d353c422ae1457763142729d0f0
Your Name <you@example.com>
2025-11-11 19:02:01 -0600

    Resolve index.tsx merge conflict

--- src/src/index.tsx
+++ src/src/index.tsx
@@ -1,4 +1,18 @@
-<<<<<<< HEAD
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import AIPromptVault from "./AIPromptVault";
+
+const container = document.getElementById("root");
+
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <AIPromptVault />
+    </React.StrictMode>
+  );
+}<<<<<<< HEAD
 import React from "react";
 import { createRoot } from "react-dom/client";
 import AIPromptVault from "./AIPromptVault";
