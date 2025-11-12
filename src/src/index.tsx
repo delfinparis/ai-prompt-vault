b282b3caf3b92d75f663b179a97bafc752237cde
Your Name <you@example.com>
2025-11-11 19:04:04 -0600

    Clean up index.tsx merge conflict

--- src/src/index.tsx
+++ src/src/index.tsx
@@ -12,32 +12,4 @@ if (container) {
       <AIPromptVault />
     </React.StrictMode>
   );
-}<<<<<<< HEAD
-import React from "react";
-import { createRoot } from "react-dom/client";
-import AIPromptVault from "./AIPromptVault";
-import "./App.css";
-
-const root = createRoot(document.getElementById("root")!);
-root.render(<AIPromptVault />);
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
->>>>>>> 0511aae (Initialize project using Create React App)
+}
